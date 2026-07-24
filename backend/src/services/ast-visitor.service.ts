42286825f5168fcd7a393a74ca782f6272bbdafd
vansharorx <vansharora21102005@gmail.com>
2026-07-24 11:43:29 +0530

    feat(ast): migrate class discovery to single-pass visitor

--- backend/src/services/ast-visitor.service.ts
+++ backend/src/services/ast-visitor.service.ts
@@ -23,53 +23,119 @@ export const visitAST = (
 
         if (ts.isFunctionDeclaration(node)) {
 
-        const start =
-            sourceFile.getLineAndCharacterOfPosition(
-                node.getStart()
-            );
+            const start =
+                sourceFile.getLineAndCharacterOfPosition(
+                    node.getStart()
+                );
 
-        const end =
-            sourceFile.getLineAndCharacterOfPosition(
-                node.getEnd()
-            );
+            const end =
+                sourceFile.getLineAndCharacterOfPosition(
+                    node.getEnd()
+                );
 
-        analysis.functions.push({
+            analysis.functions.push({
 
-            name:
-                node.name?.text ?? "anonymous",
+                name:
+                    node.name?.text ?? "anonymous",
 
-            async:
-                node.modifiers?.some(
-                    modifier =>
-                        modifier.kind ===
-                        ts.SyntaxKind.AsyncKeyword
-                ) ?? false,
+                async:
+                    node.modifiers?.some(
+                        modifier =>
+                            modifier.kind ===
+                            ts.SyntaxKind.AsyncKeyword
+                    ) ?? false,
 
-            exported:
-                node.modifiers?.some(
-                    modifier =>
-                        modifier.kind ===
-                        ts.SyntaxKind.ExportKeyword
-                ) ?? false,
+                exported:
+                    node.modifiers?.some(
+                        modifier =>
+                            modifier.kind ===
+                            ts.SyntaxKind.ExportKeyword
+                    ) ?? false,
 
-            parameters:
-                node.parameters.map(parameter =>
-                    parameter.name.getText(sourceFile)
-                ),
+                parameters:
+                    node.parameters.map(parameter =>
+                        parameter.name.getText(sourceFile)
+                    ),
 
-            returnType:
-                node.type?.getText(sourceFile) ??
-                null,
+                returnType:
+                    node.type?.getText(sourceFile) ??
+                    null,
 
-            startLine:
-                start.line + 1,
+                startLine:
+                    start.line + 1,
 
-            endLine:
-                end.line + 1,
+                endLine:
+                    end.line + 1,
 
-        });
+            });
 
-    }
+        }
+
+        if (ts.isClassDeclaration(node)) {
+
+            const start =
+                sourceFile.getLineAndCharacterOfPosition(
+                    node.getStart()
+                );
+
+            const end =
+                sourceFile.getLineAndCharacterOfPosition(
+                    node.getEnd()
+                );
+
+            const extendsClause =
+                node.heritageClauses?.find(
+                    clause =>
+                        clause.token ===
+                        ts.SyntaxKind.ExtendsKeyword
+                );
+
+            const implementsClause =
+                node.heritageClauses?.find(
+                    clause =>
+                        clause.token ===
+                        ts.SyntaxKind.ImplementsKeyword
+                );
+
+            analysis.classes.push({
+
+                name:
+                    node.name?.text ?? "anonymous",
+
+                exported:
+                    node.modifiers?.some(
+                        modifier =>
+                            modifier.kind ===
+                            ts.SyntaxKind.ExportKeyword
+                    ) ?? false,
+
+                abstract:
+                    node.modifiers?.some(
+                        modifier =>
+                            modifier.kind ===
+                            ts.SyntaxKind.AbstractKeyword
+                    ) ?? false,
+
+                extends:
+                    extendsClause?.types[0]
+                        ?.expression.getText(sourceFile) ??
+                    null,
+
+                implements:
+                    implementsClause?.types.map(
+                        type =>
+                            type.expression.getText(sourceFile)
+                    ) ?? [],
+
+                startLine:
+                    start.line + 1,
+
+                endLine:
+                    end.line + 1,
+
+            });
+
+        }
 
         ts.forEachChild(
             node,
